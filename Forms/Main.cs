78f89001347b497188f1f185ae2309ec9f8f9b1d
thewerthon <eeeeewerton@gmail.com>
2025-01-09 10:44:25 -0300

    Initial Commit

---
+++ Forms/Main.cs
@@ -0,0 +1,10 @@
+namespace VideoConverter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+namespace VideoConverter;
+
+public static class Program {
+
+	[STAThread]
+	static void Main() {
+
+		ApplicationConfiguration.Initialize();
+		Application.Run(new Main());
+
+	}
+
+}
